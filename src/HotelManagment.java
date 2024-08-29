4b1233a720ee572a58682fe838e8c22043da653b
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-08-29 15:14:48 +0200

    room management

--- src/HotelManagment.java
+++ src/HotelManagment.java
@@ -12,6 +12,13 @@ public class HotelManagment {
         Clients.add(client3);
         Hotel hotel = new Hotel("HotelAl-Adarissa", 4, Reservations, Clients);
         hotel.displayClients();
+        hotel.makeReservation(client1,1,"29-08-2024", "30-08-2024");
+        hotel.makeReservation(client2,2,"29-08-2024", "31-08-2024");
+        hotel.makeReservation(client2,1,"29-08-2024", "31-08-2024");
+
+//        hotel.editeReservation(client2,2,"22-22-22", "23-23-23");
+//        hotel.deleteReservation(client2,2);
+//        hotel.displayReservations();
 
     }
 }
@@ -19,14 +26,44 @@ public class HotelManagment {
 class Reservation {
     private int reservationId;
     private String clientName;
-    private int roomNumber;
+    private int roomToReserve;
     private String startDate;
     private String endDate;
-    public Reservation(int reservationId ,String clientName, int roomNumber, String startDate,String endDate){
+    public Reservation(int reservationId ,String clientName, int roomToReserve, String startDate,String endDate){
         this.reservationId = reservationId;
         this.clientName = clientName;
-        this.roomNumber = roomNumber;
+        this.roomToReserve = roomToReserve;
         this.startDate = startDate;
+        this.endDate = endDate;
+    }
+    public int getReservationId(){
+        return reservationId;
+    }
+    public String getClientName(){
+        return clientName;
+    }
+    public int roomToReserve(){
+        return roomToReserve;
+    }
+    public String getStartDate(){
+        return startDate;
+    }
+    public String getEndDate(){
+        return endDate;
+    }
+    public void setReservationId(int reservationId){
+        this.reservationId = reservationId;
+    }
+    public void setClientName(String clientName){
+        this.clientName = clientName;
+    }
+    public void setroomToReserve(int roomToReserve){
+        this.roomToReserve = roomToReserve;
+    }
+    public void setStartDate(String startDate){
+        this.startDate = startDate;
+    }
+    public void setEndDate(String endDate){
         this.endDate = endDate;
     }
 
@@ -48,6 +85,48 @@ class Hotel{
             System.out.println("client name : "+ client.getName() + " \nclient age : " + client.getAge() +".");
         }
     }
+    public void makeReservation(Client client ,int roomToReserve,String startDate,String endDate){
+        int reservationId = Reservations.size()+1;
+        if(numberOfRooms >= roomToReserve){
+            Reservation reservation = new Reservation(reservationId,client.getName(),roomToReserve,startDate,endDate);
+            Reservations.add(reservation);
+             numberOfRooms -=  roomToReserve;
+            System.out.println("Reservation done successfully");
+        }else{
+            System.out.println("there is no enough rooms ");
+        }
+
+    }
+    public void displayReservations(){
+        System.out.println("reservations informations: ");
+        for(Reservation reservation : Reservations){
+            System.out.println("reservation ID : " + reservation.getReservationId() + "\nclient name : " + reservation.getClientName() + "\nreservation start date : " + reservation.getStartDate() + "\nreservation end date : " + reservation.getEndDate());
+        }
+    }
+
+    public void editeReservation(Client client , int reservationId,String newStartDate ,String newEndDate){
+        for(Reservation reservation : Reservations){
+            if(reservation.getReservationId() == reservationId && reservation.getClientName().equals(client.getName())){
+                reservation.setStartDate(newStartDate);
+                reservation.setEndDate(newEndDate);
+                System.out.println("Reservation edited successfully");
+            }else{
+                System.out.println("Reservation does not exist");
+            }
+        }
+    }
+
+    public void deleteReservation(Client client , int reservationId){
+        for(Reservation reservation : Reservations){
+            if(reservation.getReservationId() == reservationId && reservation.getClientName().equals(client.getName())){
+                Reservations.remove(reservation);
+                return;
+            }else{
+                System.out.println("Reservation does not exist");
+            }
+        }
+    }
+
 
 }
 
@@ -64,5 +143,6 @@ class Client{
     public int getAge(){
         return age;
     }
+
 }
 
